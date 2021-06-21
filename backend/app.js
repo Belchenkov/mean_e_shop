213993b56bf4e2726b4179bb0a314bc6e24504cd
Aleksey Belchenkov <u608110@gmail.com>
2021-06-21 08:40:12 +0400

    Parse body json

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,25 @@ const app = express();
 // env
 const PORT = process.env.PORT || 3005;
 const HOST = process.env.HOST || 'http://localhost';
-const api = process.env.API_URL;
+const api = process.env.API_URL || '';
+
+// middlewares
+app.use(express.json());
 
 app.get(`${api}/products`, (req, res) => {
-    res.send('MERN E-Shop');
-})
+    const product = {
+        id: 1,
+        name: "hair dresser",
+        image: 'url'
+    }
+    res.send(product);
+});
+
+app.post(`${api}/products`, (req, res) => {
+    const newProduct = req.body;
+
+    res.send(newProduct);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running ${HOST}:${PORT}`);

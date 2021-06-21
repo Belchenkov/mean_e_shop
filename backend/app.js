8ec8f4079e3c13d6f7189a7b572ad454a56cd442
Aleksey Belchenkov <u608110@gmail.com>
2021-06-21 09:37:12 +0400

    Connect to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 require('dotenv/config');
 const app = express();
@@ -7,13 +8,15 @@ const app = express();
 // env
 const PORT = process.env.PORT || 3005;
 const HOST = process.env.HOST || 'http://localhost';
-const api = process.env.API_URL || '';
+const MONGO_URL = process.env.MONGO_URL;
+const DB_NAME = process.env.DB_NAME;
+const API = process.env.API_URL;
 
 // middlewares
 app.use(express.json());
 app.use(morgan('tiny'));
 
-app.get(`${api}/products`, (req, res) => {
+app.get(`${API}/products`, (req, res) => {
     const product = {
         id: 1,
         name: "hair dresser",
@@ -22,12 +25,23 @@ app.get(`${api}/products`, (req, res) => {
     res.send(product);
 });
 
-app.post(`${api}/products`, (req, res) => {
+app.post(`${API}/products`, (req, res) => {
     const newProduct = req.body;
 
     res.send(newProduct);
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running ${HOST}:${PORT}`);
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: DB_NAME
+}).then(() => {
+    console.log('DB Connection is ready...')
+
+    app.listen(PORT, () => {
+        console.log(`Server is running ${HOST}:${PORT}`);
+    });
+}).catch(err => {
+    console.log(err);
 });
+

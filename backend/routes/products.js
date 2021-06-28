be411599c93a6729f2686627bed5ed4ca78ac6cb
Aleksey Belchenkov <u608110@gmail.com>
2021-06-28 09:16:33 +0400

    update product by id

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -5,7 +5,8 @@ const Product = require('../models/product');
 const Category = require('../models/category');
 
 router.get('/', async (req, res) => {
-    const products = await Product.find({});
+    const products = await Product.find({})
+        .populate('category');
 
     if (!products) {
         res.status(500).json({ success: false });
@@ -16,7 +17,8 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     const { id } = req.params;
 
-    const product = await Product.findById(id);
+    const product = await Product.findById(id)
+        .populate('category');
 
     if (!product) {
         return res.status(404).send('The product is not found!');
@@ -81,8 +83,65 @@ router.post('/', async (req, res) => {
             success: false
         });
     }
+});
 
+router.put(`/:id`, async (req, res) =>{
+    const { id } = req.params;
+    const {
+        name,
+        description,
+        richDescription,
+        image,
+        brand,
+        price,
+        category,
+        countInStock,
+        rating,
+        numReview,
+        isFeatured
+    } = req.body;
 
+    try {
+        const checkCategory = await Category.findById(category);
+
+        if (!checkCategory) {
+            return res.status(404).send('Invalid category!');
+        }
+
+        const product = await Product.findByIdAndUpdate(
+            id,
+            {
+                name,
+                description,
+                richDescription,
+                image,
+                brand,
+                price,
+                category,
+                countInStock,
+                rating,
+                numReview,
+                isFeatured
+            }, { new: true });
+
+        if(!product) {
+            res.status(404).json({
+                success: false,
+                message: 'The product cannot by updated!'
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            product
+        });
+    } catch (error) {
+        console.error(error.name + ': ' + error.message);
+        return res.status(400).json({
+            success: false,
+            error
+        });
+    }
 });
 
 module.exports = router;

03926954ac998364c6c28f8ff37b20b967a705b3
Aleksey Belchenkov <u608110@gmail.com>
2021-06-30 08:55:36 +0400

    Create new user route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -12,4 +12,51 @@ router.get('/', async (req, res) => {
     res.send(users);
 });
 
+router.post('/', async (req, res) => {
+    const {
+        name,
+        email,
+        passwordHash,
+        phone,
+        isAdmin,
+        street,
+        apartment,
+        zip,
+        city,
+        country
+    } = req.body;
+
+    try {
+        const user = new User({
+            name,
+            email,
+            passwordHash,
+            phone,
+            isAdmin,
+            street,
+            apartment,
+            zip,
+            city,
+            country
+        });
+
+        const newUser = await user.save();
+
+        if (!newUser) {
+            return res.status(404).send('The user cannot be created!');
+        }
+
+        res.status(201).json({
+            success: true,
+            newUser
+        });
+    } catch (error) {
+        console.error(error.name + ': ' + error.message);
+        return res.status(400).json({
+            success: false,
+            error
+        });
+    }
+});
+
 module.exports = router;

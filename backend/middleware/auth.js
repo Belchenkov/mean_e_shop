7bc5a65c045a61ad242620c3de9adba33ca15dd4
Aleksey Belchenkov <u608110@gmail.com>
2021-07-02 14:22:31 +0400

    excluding rest api routes from auth

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,10 +2,41 @@ const expressJwt = require('express-jwt');
 
 const authJwt = () => {
     const secret = process.env.JWT_SECRET;
+    const api = process.env.API_URL;
 
     return expressJwt({
         secret,
         algorithms: ['HS256']
+    }).unless({
+        path: [
+            {
+                url: `${api}/users/login`,
+                methods: [
+                    'POST'
+                ]
+            },
+            {
+                url: `${api}/users/register`,
+                methods: [
+                    'POST'
+                ]
+            },
+            {
+                url: /\/api\/v1\/products(.*)/,
+                methods: [
+                    'GET',
+                    'OPTIONS'
+                ]
+            },
+            {
+                url: /\/api\/v1\/categories(.*)/,
+                methods: [
+                    'GET',
+                    'OPTIONS'
+                ]
+            },
+
+        ]
     });
 };
 

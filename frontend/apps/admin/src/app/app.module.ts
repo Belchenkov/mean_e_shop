82dd6d3828de24b8a8f23328bece68d65b55ce32
Алексей Бельченков <u608110@gmail.com>
2022-01-10 11:14:44 +0400

    HTML Editor for rich description field

--- frontend/apps/admin/src/app/app.module.ts
+++ frontend/apps/admin/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { InputSwitchModule } from 'primeng/inputswitch';
 import { DropdownModule } from 'primeng/dropdown';
+import { EditorModule } from 'primeng/editor';
 
 import { AppComponent } from './app.component';
 import { CategoriesService } from '@frontend/products';
@@ -42,6 +43,7 @@ const UX_MODULE = [
   InputTextareaModule,
   InputSwitchModule,
   DropdownModule,
+  EditorModule,
 ];
 
 const routes: Routes = [

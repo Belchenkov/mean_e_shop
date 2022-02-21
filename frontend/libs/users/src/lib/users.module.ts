e7b7502d2bf47febc7daadfeec4843bd9c6d8fdb
Алексей Бельченков <u608110@gmail.com>
2022-02-21 12:23:45 +0400

    Login Page

--- frontend/libs/users/src/lib/users.module.ts
+++ frontend/libs/users/src/lib/users.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Route } from '@angular/router';
+import { RouterModule, Route, Routes } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { LoginComponent } from './pages/login/login.component';
 
 export const usersRoutes: Route[] = [];
 
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  }
+];
+
 @NgModule({
-    imports: [CommonModule, RouterModule]
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    InputTextModule,
+    ButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+    declarations: [
+      LoginComponent
+    ]
 })
 export class UsersModule {}

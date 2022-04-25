14c973552ba17226fd8f92be79153a55babae6cb
Алексей Бельченков <u608110@gmail.com>
2022-04-25 11:12:58 +0400

    Animate Banner

--- frontend/libs/ui/src/lib/ui.module.ts
+++ frontend/libs/ui/src/lib/ui.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BannerComponent } from './components/banner/banner.component';
 import { ButtonModule } from 'primeng/button';
+
+import { BannerComponent } from './banner/banner.component';
+
 
 @NgModule({
     imports: [CommonModule, ButtonModule],

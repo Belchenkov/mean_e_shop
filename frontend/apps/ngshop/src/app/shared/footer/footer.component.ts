3f80bb5ab011d38044120c78e887b2e48d00420a
Алексей Бельченков <u608110@gmail.com>
2022-09-12 19:21:36 +0400

    Refactor project

--- frontend/apps/ngshop/src/app/shared/footer/footer.component.ts
+++ frontend/apps/ngshop/src/app/shared/footer/footer.component.ts
@@ -1,18 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'eshop-footer',
   templateUrl: './footer.component.html'
 })
-export class FooterComponent implements OnInit {
-
-  constructor() { }
-
+export class FooterComponent {
   get lastYear(): number {
     return new Date().getFullYear();
   }
-
-  ngOnInit(): void {
-  }
-
 }
